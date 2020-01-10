9b7372c254ce323a7250794a6f1618e920be6f43
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2020-01-11 01:57:13 +0500

    Add documentToReactComponents and change data

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,50 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout/layout'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+// export const query = graphql`
+//     query ($slug: String!) {
+//         markdownRemark(fields: {
+//             slug: {
+//                 eq: $slug
+//             }
+//         }) {
+//             frontmatter {
+//                 title
+//                 date
+//             }
+//             html
+//         }
+//     }`
 
 export const query = graphql`
-    query ($slug: String!) {
-        markdownRemark(fields: {
-            slug: {
-                eq: $slug
-            }
-        }) {
-            frontmatter {
-                title
-                date
-            }
-            html
+    query($slug: String!){
+        contentfulBlogPost(slug: {eq: $slug}) {
+        title
+        publishDate(formatString: "D MMMM, YYYY", locale:"ru")
+        content {
+            json
         }
-    }`
+    }
+}
+`
 
 const Blog = (props) => {
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title['en-US']
+                const url = node.data.target.fields.file['en-US'].url
+                return <img alt={alt} src={url} />
+            }
+        }
+    }
     return (
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.content.json, options)}
         </Layout>
     )
 }
