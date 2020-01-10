080b752a07a48a7d3fc10bf71bd317861cd906cd
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2020-01-11 01:53:57 +0500

    Install dotenv and g-s-contentful plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}`, })
+
 module.exports = {
   siteMetadata: {
     title: `My blog`,
@@ -6,6 +8,14 @@ module.exports = {
     menuItems: [{ name: 'Самое свежее', page: 'newest' }, { name: 'Посты', page: 'blog' }, { name: 'Лучшее', page: 'the-best' }, { name: 'Обо мне', page: 'about' }]
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
