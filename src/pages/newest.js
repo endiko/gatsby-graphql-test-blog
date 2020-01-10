7372936fac590c66650390912b0030bbba2bb6c8
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2020-01-10 19:06:23 +0500

    Create newest page

---
+++ src/pages/newest.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+const Newest = (props) => {
+    return (
+        <Layout>
+            <h1>Newest page</h1>
+        </Layout>
+    )
+}
+
+export default Newest
