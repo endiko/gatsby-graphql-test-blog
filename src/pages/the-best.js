a1eb40e2c196aede3355af94a62f30f4ce659858
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2020-01-10 19:07:02 +0500

    Create the best page

---
+++ src/pages/the-best.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+const TheBest = (props) => {
+    return (
+        <Layout>
+            <h1>The Best page</h1>
+        </Layout>
+    )
+}
+
+export default TheBest
