9c965370cce544b82abb955aa81904418b31eb18
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2020-01-12 22:57:36 +0500

    Add social icons

---
+++ src/components/socials/socialIcons.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import SocialIcon from './socialIcon'
+import './socialIcons.scss'
+import { FaInstagram, FaYoutube } from 'react-icons/fa';
+
+const SocialIcons = (props) => {
+    return (
+        <ul className="social-icon__list">
+            <li className="social-icon__item">
+                <a href={props.socials[0].href} className="social-icon__item--link" data-tooltip={props.socials[0].name}>
+                    <FaInstagram className="social-icon" />
+                </a>
+            </li>
+            <li className="social-icon__item">
+                <a href={props.socials[1].href} className="social-icon__item--link" data-tooltip={props.socials[1].name}>
+                    <FaYoutube className="social-icon" />
+                </a>
+            </li>
+        </ul>
+    )
+}
+
+export default SocialIcons;
